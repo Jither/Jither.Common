48a3309c20990bcf067ff4d40ccea0b7c8f8a556
Jither <jimmith@gmail.com>
2021-04-20 22:38:16 +0200

    PathHelper: MakeValidFileName

---
+++ Utilities/PathHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Jither.Utilities
+{
+    public static class PathHelpers
+    {
+        private static HashSet<char> InvalidCharacters = new HashSet<char>(Path.GetInvalidFileNameChars());
+
+        /// <summary>
+        /// Replaced invalid filename characters with a replacement character.
+        /// </summary>
+        /// <remarks>This is only intended as a first line defense. There are many other invalid filename possibilities, some depending on context.</remarks>
+        public static string MakeValidFileName(string fileName, char replacement = '_')
+        {
+            char[] result = new char[fileName.Length];
+            for (int i = 0; i < fileName.Length; i++)
+            {
+                char c = fileName[i];
+                if (InvalidCharacters.Contains(c))
+                {
+                    c = replacement;
+                }
+                result[i] = c;
+            }
+            return new String(result);
+        }
+    }
+}
